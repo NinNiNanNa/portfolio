5e246783bbe0d15580b18e6f83e265c9df123000
NinNiNanNa <kimsojin3011@gmail.com>
2024-04-11 22:18:26 +0900

    Projects 컴포넌트 데이터 불러오기

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,29 @@
+import { useQuery } from "react-query";
+import { IGetResult, getProjects } from "../services/projects";
+/* CSS import */
+import style from "../styles/Projects.module.css";
+
 function Projects() {
-  return <div>Projects</div>;
+  const { data, isLoading } = useQuery<IGetResult>({
+    queryKey: ["projects"],
+    queryFn: () => getProjects(),
+  });
+  console.log(data);
+
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className={style.wrap}>
+          <h1>💻 It's my works! 💻</h1>
+          {data?.projects.map((item, idx) => (
+            <div key={idx}>{item.name}</div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Projects;
---
+++ src/services/projects.ts
@@ -0,0 +1,25 @@
+const DATA_PATH = process.env.REACT_APP_DATA_PATH;
+
+interface IList {
+  type: string;
+  name: string;
+  img: string;
+  description: string;
+  tags: string[];
+  source_code?: string;
+  detail_notion?: string;
+  detail_url?: string;
+  drive_url?: string;
+  distribute: string;
+  created_year: string;
+}
+
+export interface IGetResult {
+  projects: IList[];
+}
+
+export function getProjects() {
+  return fetch(`https://ninninanna.github.io/${DATA_PATH}`).then((res) =>
+    res.json()
+  );
+}
